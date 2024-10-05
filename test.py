bd32ccb3808e939d886c915353d57348d0540b98
Linn Bergman <juliuspettersson04@gmail.com>
2024-10-05 04:35:23 +0200

    Added tests

---
+++ test.py
@@ -0,0 +1,29 @@
+from compiler.lexer import Lexer
+from compiler.notToken import *
+from compiler.parser import Parser
+from compiler.emitter import Emitter
+import unittest
+
+class testLexer(unittest.TestCase):
+    def testEmpty(self):
+        lexer = Lexer("")
+        tokens = lexer.getTokens()
+        enums_list = [token.type for token in tokens]
+        self.assertEqual(enums_list, [TokenType.NEWLINE, TokenType.NEWLINE])
+
+    def testPrintString(self):
+        lexer = Lexer('PRINT "hello world!"')
+        tokens = lexer.getTokens()
+        enums_list = [token.type for token in tokens]
+        awnser = [TokenType.PRINT, TokenType.STRING, TokenType.NEWLINE, TokenType.NEWLINE] 
+        self.assertEqual(enums_list, awnser)
+
+    def testPrintVariable(self):
+        lexer = Lexer('PRINT variable')
+        tokens = lexer.getTokens()
+        enums_list = [token.type for token in tokens]
+        awnser =  [TokenType.PRINT, TokenType.IDENT, TokenType.NEWLINE]
+        self.assertEqual(enums_list, awnser)
+
+
+unittest.main()
