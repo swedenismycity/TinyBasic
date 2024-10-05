7de4302bb9d57b9d5ff36c94f5bbc185ee3d83db
Linn Bergman <juliuspettersson04@gmail.com>
2024-10-05 02:15:35 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from compiler.notToken import *
 from compiler.parser import Parser
 from compiler.emitter import Emitter
 
-#TODO
-#MAKE GOOD EROOR HANDLING (kinda done) 
-#MAKE EMITER
-#add the ability to make comments 
-
 source = open('basicFile.txt', 'r').read()
 
 lexer = Lexer(source)
